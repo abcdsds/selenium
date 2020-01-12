6be8caed133c2ef06b729dadce38a5e43f4ddcbe
ds <ds@DESKTOP-0F7GVLF>
2020-01-12 18:15:49 +0900

    버전1

--- src/test/java/ond/test/test/seleniumtest.java
+++ src/test/java/ond/test/test/seleniumtest.java
@@ -8,7 +8,9 @@ import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -38,6 +40,11 @@ public class seleniumtest {
 ////		WebElement iframe = driver.findElement(By.tagName("iframe")); //태그이름으로 Element 가져오기
 //
 //	}
+	
+	public void proxyCheck() {
+		
+		
+	}
 
 	//@Test
 	public void join() throws InterruptedException {
@@ -87,7 +94,7 @@ public class seleniumtest {
 	}
 	
 	
-	@Test
+	//@Test
 	public void login() throws InterruptedException {
 		// TODO Auto-generated method stub
 
@@ -110,33 +117,47 @@ public class seleniumtest {
 		//driver.switchTo().frame(driver.findElement(By.xpath("iframe[title='온디스크']")));	
 		//driver.switchTo().frame(driver.findElement(By.xpath(".//iframe[@src='http://ondisk.co.kr/index.php']")));
 
-		//input[@id='id_str']
 		
 		driver.findElement(By.xpath("//input[@id='mb_id']")).click();
 		Thread.sleep(500);
 		driver.findElement(By.xpath("//input[@id='mb_id']")).sendKeys("ansrl0099");
-//		Thread.sleep(500);
-//		driver.findElement(By.xpath("//input[@id='pw_str1']")).click();
-//		Thread.sleep(500);
-//		driver.findElement(By.xpath("//input[@id='pw_str1']")).sendKeys("123123");
-//		Thread.sleep(500);
-//		driver.findElement(By.xpath("//input[@id='pw_str2']")).click();
-//		Thread.sleep(500);
-//		driver.findElement(By.xpath("//input[@id='pw_str2']")).sendKeys("123123");
-//		Thread.sleep(500);
-//		driver.findElement(By.xpath("//input[@id='nic_str']")).click();
-//		driver.findElement(By.xpath("//input[@id='nic_str']")).sendKeys("ansrl8716");
-//		
-//		driver.findElement(By.xpath("//input[@id='all_check']")).click();
-//		Thread.sleep(1000);
-//		driver.findElement(By.xpath("//input[@id='submitBtn']")).click();
-//		
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//input[@name='mb_pw']")).click();
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//input[@name='mb_pw']")).sendKeys("123123");
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//input[@name='mb_pw']")).sendKeys(Keys.ENTER);
 		
-//		System.out.println(driver.findElement(By.xpath("//img[@alt='무료회원가입 1000포인트 지급']")));
-//		assertThat(driver.getTitle(),is("온디스크"));
-//		assertThat(driver.getCurrentUrl(),is("http://ondisk.co.kr/index.php?mode=join"));
-//		System.out.println("2########################");
-		//driver.close();
+		Thread.sleep(1000);
+		
+		driver.switchTo().alert().accept();
+		
+		Thread.sleep(500);
+		
+		driver.navigate().to("http://ondisk.co.kr/pop.php?sm=special_coupon");
+		
+		
+		assertThat(driver.getCurrentUrl(),is("http://ondisk.co.kr/pop.php?sm=special_coupon"));
+		
+		String[] coupon = "L78W-NYFX-E8TYRG".split("-");
+				
+		
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//input[@id='coupon_no1']")).click();
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//input[@id='coupon_no1']")).sendKeys(coupon[0]);
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//input[@id='coupon_no2']")).sendKeys(coupon[1]);
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//input[@id='coupon_no3']")).sendKeys(coupon[2]);
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//img[contains(@src,'http://image.ondisk.co.kr/img/popup/special_coupon/btn.gif')]")).click();
+		Thread.sleep(500);
+		Alert alert = driver.switchTo().alert();
+		if (alert.getText().indexOf("IP") != -1 ) {
+			
+		}
+		
 //		WebElement coolestWidgetEvah = driver.findElement(By.id("coolestWidgetEvah")); //id로 Element 가져오기
 //		WebElement cheese = (WebElement) driver.findElements(By.className("cheese")); //클래스이름으로 Element 가져오기
 //		WebElement iframe = driver.findElement(By.tagName("iframe")); //태그이름으로 Element 가져오기
